775238869f4bdf6d25608e5f27ad0eb43870f97f
balaangular <mkb40252627@gmail.com>
2020-08-28 12:22:59 +0530

    confl222

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/test', pathMatch: 'full'},
+  {path: 'test', component: DynamicFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
